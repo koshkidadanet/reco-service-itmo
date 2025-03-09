07a9ba8821a34d24a65e136ebb546bbd788b6548
Denis Agapitov <koshkidadanet@gmail.com>
2025-03-09 14:13:50 +0500

    flake8

--- service/api/views.py
+++ service/api/views.py
@@ -53,7 +53,7 @@ async def get_reco(
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
-    # Тут надо проверять, что юзер в числе тех, что есть в датасете, 
+    # Тут надо проверять, что юзер в числе тех, что есть в датасете,
     # если мы не готовы работать с холодными
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
