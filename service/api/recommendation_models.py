57fd3beac53ffde6b99f26d9fa4036c6e783cb19
Denis Agapitov <koshkidadanet@gmail.com>
2025-04-13 14:10:12 +0500

    new line

--- service/api/recommendation_models.py
+++ service/api/recommendation_models.py
@@ -133,4 +133,3 @@ class RangeModel:
         if self.logger:
             self.logger.debug(f"Generating range recommendations for user {user_id} with k={k_recs}")
         return list(range(k_recs))
-    
